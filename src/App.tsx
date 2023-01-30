a9a033f581405a9be860a1de3a9a52baf6a8ad70
naekit <mykyta.onishchuck@code.berlin>
2023-01-30 12:43:58 -0500

    added formelement type definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
+import NewTodo from "./components/NewTodo"
 import Todos from "./components/Todos"
 import Todo from "./models/todo"
 
 function App() {
-	const todos = [new Todo("Learn React"), new Todo("Learn Typescript")]
+	const [todos, setTodos] = useState<Todo[]>([])
+
+	const addTodo = (text: string) => {
+		const newTodo = new Todo(text)
+		setTodos((prevTodos) => prevTodos.concat(newTodo))
+	}
 
 	return (
 		<div className="App">
+			<NewTodo onAddTodo={addTodo} />
 			<Todos items={todos} />
 		</div>
 	)
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,26 @@
-import React from "react"
+import React, { useRef } from "react"
 
-const NewTodo = () => {
-	return <div>NewTodo</div>
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+	const textInputRef = useRef<HTMLInputElement>(null)
+
+	const submitHandler = (event: React.FormEvent) => {
+		event.preventDefault()
+		const enteredText = textInputRef.current!.value
+		if (enteredText.trim().length === 0) {
+			// throw an error
+			return
+		}
+
+		props.onAddTodo(enteredText)
+	}
+
+	return (
+		<form onSubmit={submitHandler}>
+			<label htmlFor="text">Todo text</label>
+			<input type="text" id="text" ref={textInputRef} />
+			<button type="submit">Add Todo</button>
+		</form>
+	)
 }
 
 export default NewTodo
